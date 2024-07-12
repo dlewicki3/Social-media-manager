8d23e955ddddba0c11e73b7f86c928395fb21f7a
Dominik Lewicki <dominik_03-06@wp.pl>
2024-07-12 16:10:06 +0100

    ggg

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import O1 from "./O1";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+<O1/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/O1.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./O1.css";
+
+function O1() {
+  return (
+   <div className="o1">
+    <div className="k1">
+<div className="content">SOCIAL MEDIA MANAGER PORTFOLIO</div>
+<div className="under-contnet">
+  <div className='blabla-txt-div'>Dominik Lewicki
+    <br></br>
+    Social Media Specialist
+    <br></br>
+    & Front-End Developer
+  </div>
+</div>
+    </div>
+   </div>
+  )
+}
+
+export default O1
