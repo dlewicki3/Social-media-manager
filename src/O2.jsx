828320fcaa740f9cca064a985b25d886ac3ac2f5
Dominik Lewicki <dominik_03-06@wp.pl>
2024-07-12 21:27:15 +0100

    ffff

--- src/O2.jsx
+++ src/O2.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./O2.css";
+import Cos from './Pro.svg';
 
 function O2() {
   return (
  <div className="o2">
-    <div className="k2"></div>
+    <div className="k2">
+        <div className="left2">
+            <img className='foto' src = {Cos}></img>
+        </div>
+        <div className="right2">
+            <div className="top2">ABOUT ME</div>
+            <div className="bottom2">
+            Witam, jestem Dominik Lewicki, specjalizuję się w tworzeniu stron internetowych i zarządzaniu mediami społecznościowymi. Sztuczna inteligencja jest kluczowym elementem mojej pracy.
+            </div>
+        </div>
+    </div>
  </div>
   )
 }
