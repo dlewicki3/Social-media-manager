39412415e0ef6cf476271ddf07c7807abcdacf68
Dominik Lewicki <dominik_03-06@wp.pl>
2024-07-12 17:26:56 +0100

    hfhf

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import O1 from "./O1";
+import O2 from "./O2";
 import Navbar from "./Navbar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar/>
 <O1/>
+<O2/>
 
     </div>
   );
---
+++ src/O2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./O2.css";
+
+function O2() {
+  return (
+ <div className="o2">
+    <div className="k2"></div>
+ </div>
+  )
+}
+
+export default O2
