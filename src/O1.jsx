9ed770ff205c9617e7b5396d841726ad01d2e0a1
Dominik Lewicki <dominik_03-06@wp.pl>
2024-07-13 17:37:11 +0100

    fff

--- src/O1.jsx
+++ src/O1.jsx
@@ -1,22 +1,61 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
 import "./O1.css";
 
 function O1() {
+  useEffect(() => {
+    gsap.fromTo(
+      ".big-txt1", 
+      { y: 50, opacity: 0 }, 
+      { y: 0, opacity: 1, duration: 0.5, delay: 0.1 }
+    );
+    gsap.fromTo(
+      ".big-txt2", 
+      { y: 50, opacity: 0 }, 
+      { y: 0, opacity: 1, duration: 0.5, delay: 0.3 }
+    );
+    gsap.fromTo(
+      ".big-txt3", 
+      { y: 50, opacity: 0 }, 
+      { y: 0, opacity: 1, duration: 0.5, delay: 0.5 }
+    );
+    gsap.fromTo(
+      ".small-txt1", 
+      { y: 50, opacity: 0 }, 
+      { y: 0, opacity: 1, duration: 0.5, delay: 0.7 }
+    );
+    gsap.fromTo(
+      ".small-txt2", 
+      { y: 50, opacity: 0 }, 
+      { y: 0, opacity: 1, duration: 0.5, delay: 0.9 }
+    );
+    gsap.fromTo(
+      ".small-txt3", 
+      { y: 50, opacity: 0 }, 
+      { y: 0, opacity: 1, duration: 0.5, delay: 1.1 }
+    );
+  }, []);
+
   return (
-   <div className="o1">
-    <div className="k1">
-<div className="content">SOCIAL MEDIA MANAGER PORTFOLIO</div>
-<div className="under-contnet">
-  <div className='blabla-txt-div'>Dominik Lewicki
-    <br></br>
-    Social Media Specialist
-    <br></br>
-    & Front-End Developer
-  </div>
-</div>
+    <div className="o1">
+      <div className="k1">
+        <div className="content">
+          <p className='big-txt1'>SOCIAL MEDIA</p>
+          <p className='big-txt2'>MANAGER</p>
+          <p className='big-txt3'>PORTFOLIO</p>
+        </div>
+        <div className="under-content">
+          <div className='blabla-txt-div'>
+            <p className='small-txt1'>Dominik Lewicki</p>
+            <br />
+            <p className='small-txt2'>Social Media Specialist</p>
+            <br />
+            <p className='small-txt3'>& Front-End Developer</p>
+          </div>
+        </div>
+      </div>
     </div>
-   </div>
-  )
+  );
 }
 
-export default O1
+export default O1;
